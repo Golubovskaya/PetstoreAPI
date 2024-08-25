d64ae964efce2db2ef88411f4f94892948dc33e2
Golubovskaya <nattwork@gmail.com>
2024-08-25 21:59:13 +0300

    Add files via upload

---
+++ apiTest.py
@@ -0,0 +1,71 @@
+import pytest
+import requests
+import jsonschema
+from jsonschema import validate
+
+
+@pytest.mark.regression
+def test_user_operations(base_url, user_id):
+    # Update user
+    update_data = {
+        "id": user_id,
+        "username": "test_api",
+        "firstName": "test",
+        "lastName": "api",
+        "email": "test_api@email.com",
+        "password": "qwerty",
+        "phone": "+123456123456",
+        "userStatus": 0
+    }
+    update_user = requests.put(f'{base_url}/user/{user_id}', json=update_data)
+    print("Update user response" + update_user.text)
+    assert update_user.status_code == 200
+    print(update_user.headers)
+    assert update_user.headers['Content-Type'] == 'application/json'
+
+    # Get user info
+    get_user = requests.get(f'{base_url}/user/test_api')
+    print("Get user response" + get_user.text)
+    assert get_user.status_code == 200
+    print(get_user.headers)
+    assert get_user.headers['Content-Type'] == 'application/json'
+    user_info = get_user.json()
+    assert user_info['username'] == update_data['username']
+    assert user_info['phone'] == update_data['phone']
+    assert user_info['email'] == update_data['email']
+
+    # JSON schema validation
+    schema = {
+        "type": "object",
+        "properties": {
+            "id": {"type": "integer"},
+            "username": {"type": "string"},
+            "firstName": {"type": "string"},
+            "lastName": {"type": "string"},
+            "email": {"type": "string", "format": "email"},
+            "password": {"type": "string"},
+            "phone": {"type": "string"},
+            "userStatus": {"type": "integer"}
+        },
+        "required": ["id", "username", "firstName", "lastName", "email", "password", "phone", "userStatus"],
+    }
+    try:
+        validate(instance=user_info, schema=schema)
+        print("JSON schema validation success.")
+    except jsonschema.exceptions.ValidationError as e:
+        print("Error validating JSON Schema: ")
+        print(e)
+        raise e
+@pytest.mark.skip
+def test_2():
+    print("Second test")
+
+a = 321
+
+@pytest.mark.skipif(a !=234, reason = "Is not equal 234, skip test")
+def test_a():
+    assert a == 234
+
+@pytest.mark.xfail(reason = "Expected fail test")
+def test_a2():
+    assert a == 234
---
+++ conftest.py
@@ -0,0 +1,42 @@
+import pytest
+import requests
+
+@pytest.fixture(scope="module")
+def base_url():
+    base_url = 'https://petstore.swagger.io/v2'
+    return base_url
+
+@pytest.fixture(scope="module")
+def user_id():
+    user_id = 123
+    return user_id
+
+# Setup
+@pytest.fixture(scope="module", autouse=True)
+def setup(base_url, user_id):
+
+    data = {
+        "id": user_id,
+        "username": "test_api",
+        "firstName": "test",
+        "lastName": "api",
+        "email": "test_api@email.com",
+        "password": "qwerty",
+        "phone": "+123456",
+        "userStatus": 0
+    }
+    # Create user
+    create_user = requests.post(f'{base_url}/user', json=data)
+    print("Create user response" + create_user.text)
+    assert create_user.status_code == 200
+    print(create_user.headers)
+    assert create_user.headers['Content-Type'] == 'application/json'
+
+    yield
+
+    # Delete user
+    delete_user = requests.delete(f'{base_url}/user/test_api')
+    print("Delete user response" + delete_user.text)
+    assert delete_user.status_code == 200
+    print(delete_user.headers)
+    assert delete_user.headers['Content-Type'] == 'application/json'
